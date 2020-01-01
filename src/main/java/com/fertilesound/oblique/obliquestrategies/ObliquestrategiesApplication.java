d39118bbdb110d6eb89c15f2c7ff8341fa56f565
mbierma <fertilesound@yahoo.com>
2020-01-01 13:48:11 -0600

    first commit

---
+++ src/main/java/com/fertilesound/oblique/obliquestrategies/ObliquestrategiesApplication.java
@@ -0,0 +1,13 @@
+package com.fertilesound.oblique.obliquestrategies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ObliquestrategiesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ObliquestrategiesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fertilesound/oblique/obliquestrategies/ObliquestrategiesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fertilesound.oblique.obliquestrategies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ObliquestrategiesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
